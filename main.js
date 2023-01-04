7296132cbb78866efa583ed42ff07555bb60811c
Ztion <work@ztion.cn>
2023-01-04 18:24:03 +0800

    首页

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
 const { Menu, app, BrowserWindow } = require('electron')
 Menu.setApplicationMenu(null)
+require("@electron/remote/main").initialize();
+
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        transparent: true,
+        backgroundColor: '#00000000',
+        frame: false,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true
+        }
     })
+    require("@electron/remote/main").enable(win.webContents);
 
-    win.loadFile('index.html')
+    win.loadFile('./src/index.html')
+
+    win.once('ready-to-show', () => {
+        // 初始化后再显示
+        win.show()
+    })
+    //打开开发工具
+    // win.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
---
+++ src/js/data.js
@@ -0,0 +1,13 @@
+
+var app = new Vue({
+    el: '#box',
+    data: {
+        title: '首页'
+    },
+    methods: {
+        close: function (event) {
+            alert("close!")
+        }
+
+    }
+})
